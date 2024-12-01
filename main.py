2077e347a54d37ab88c10cee075eecbb4717e917
Camille PRADO <camille.prado@felisse.fr>
2024-12-01 16:15:00 +0100

    Outlined log message levels Improved handling of loading values from .env files with better error handling Moved CHECK_INTERVAL from main.py to .env file to let the user modify it Added comments to the printAndLog function to improve readability

--- main.py
+++ main.py
@@ -11,14 +11,58 @@ import json
 import ast
 from dotenv import load_dotenv
 
-#TODO Refactor the log & message code so that it's only one function.
+#TODO : Find any bugs and issues that may lurk in the code
+
+#*  -  =  -  =  -  =  -  =  -  =  -  [ - LOGS  - ]  -  =  -  =  -  =  -  =  -  =  *# 
+#*The logs are organised as such : [<timestamp> - <level>] > <text>               *#
+#*The timestamp is generated automatically and formatted as YYYY-MM-DD HH:MM:SS   *#
+#*The level is indicated by the user or the program, and is usually               *#
+#*either as follows or whatever you want it to be                                 *#
+#*INFO                  :   For information/debug purposes                        *#
+#*WARNING               :   For not vital but important informations              *#
+#*ALERT                 :   Big issue to take notice of                           *#
+#*ERROR                 :   The user misconfigured the environment file           *#
+#*  -  =  -  =  -  =  -  =  -  =  -  =  -  =  -  =  -  =  -  =  -  =  -  =  -  =  *#
+
+
+def printAndLog(text:str,level:str):
+    """
+    Log the given text to a log file, with the according level and timestamp
+    args :
+        `text`  (str)   :   the text to log to the file
+        `level` (str)   :   the level of the message. 
+                            Could be anything, but is "INFO", "WARNING", "ALERT" and "ERROR         " in the code,
+                            so you can either use that convention or do your own thing
+    """
+    message = f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {level}] > {text}"
+    print(message)
+    with open(f"{datetime.now().strftime('%Y-%m-%d')}-watcher.log", "a") as f:  # Use "a" mode to append
+        f.write(message)
+        f.close()
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get the token and user ID from the environment
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-USER_ID = int(os.getenv("USER_ID"))  # Ensure USER_ID is an integer
+if not DISCORD_TOKEN:
+    printAndLog(text="Discord token missing ! (DISCORD_TOKEN not set in the .env file)",level="ERROR")
+
+try :
+    USER_ID = int(os.getenv("USER_ID"))  # Ensure USER_ID is an integer
+    if not USER_ID:
+        printAndLog(text="User ID missing ! (USER_ID not set in the .env file)",level="ERROR")
+except ValueError:
+    printAndLog(text="User ID misconfigured ! (USER_ID not an integer in the .env file)",level="ERROR")
+
+    
+# Get the check-in interval from the .env file, with 5 as a default value
+try:
+    CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL"))
+    if not CHECK_INTERVAL:
+        printAndLog(text="Check-in interval not set, reverting to default value of 5s",level="WARNING")
+        CHECK_INTERVAL = 5
+except ValueError:
+    printAndLog(text="Check-in interval misconfigured ! (CHECK_INTERVAL not an integer in the .env file)",level="ERROR")
 
 # Extract the ports to monitor from the .env file's PORTS_TO_SCAN entry.
 # The ports are expected as a comma-separated string, which we split, 
@@ -29,12 +73,10 @@ try:
     PORTS_TO_SCAN = ast.literal_eval(os.getenv("PORTS_TO_SCAN", "{}"))
 except (ValueError, SyntaxError):
     raise ValueError("PORTS_TO_SCAN must be a valid Python dictionary.")
-
-# Ensure all keys in PORTS_TO_SCAN are integers
+# Get the list of ports to scan and their alert level
+# Ensure all keys in PORTS_TO_SCAN are integers, and map them to a dictionnary
 PORTS_TO_SCAN = {int(k): v for k, v in PORTS_TO_SCAN.items()}
 
-
-CHECK_INTERVAL = 5  # Check every 5 seconds
 
 # Create a Discord bot instance
 intents = discord.Intents.default()
@@ -42,19 +84,18 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 # Function to send a notification to Discord DMs
-async def send_dm_notification(message):
+async def send_dm_notification(message:str):
+    """
+    Sends a notification to the given user (USER_ID) with the `message` value as the content
+    args : 
+        `message` (str) : the message to send the user
+    """
     user = await bot.fetch_user(USER_ID)
     if user:
         await user.send(message)
     else:
         print(f"[ERROR] Unable to find user with ID {USER_ID}")
 
-def printAndLog(text:str,level:str):
-    message = f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {level}] > {text}"
-    print(message)
-    with open(f"{datetime.now().strftime('%Y-%m-%d')}-watcher.log", "a") as f:  # Use "a" mode to append
-        f.write(message)
-        f.close()
 
 # Function to check active connections on specified ports
 async def check_connections():
@@ -85,7 +126,7 @@ async def check_connections():
                     elif level_key == 1:
                         level = "INFO"
                     else:
-                        level = "ATTRIBUTION ERROR"
+                        level = "ERROR          "
 
                     # Craft the message using python's f-strings, with useful info like the ort number,
                     # remote IP address and port, timestamp, and of course the warning level.
@@ -107,7 +148,7 @@ async def check_connections():
 async def on_ready():
     print(f"[INFO] Logged in as {bot.user}")
     print("[INFO] Monitoring started. Press Ctrl+C to stop.")
-    await send_dm_notification(f"Bot ready at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} !")
+    await send_dm_notification(f"[INFO] Bot ready at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} !")
     bot.loop.create_task(check_connections())
 
 # Start the bot
